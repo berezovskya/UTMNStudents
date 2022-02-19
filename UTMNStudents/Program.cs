d90667de9f73c8a5f7ab3e81b01c14de7cc24d2c
berezovskya <berezovskya@hotmail.com>
2022-02-19 12:39:31 +0500

    Added iterator

--- UTMNStudents/Program.cs
+++ UTMNStudents/Program.cs
@@ -2,22 +2,20 @@
 public class Program
 {
 
-    static int rec(int x)
+    static int rec(int x, int i)
     {
+        Console.WriteLine("Recursion step # " +i);
         if (x == 0)
             return 0;
-        return rec(x - 1) + x;
+        return rec(x - 1, i + 1) + x;
     }
     static void Main(string[] args)
     {
         Console.WriteLine("INFO: Program started");
         Console.WriteLine("123");
         Console.WriteLine("HelloWorld");
-        Console.WriteLine("x = " + rec(5));
-        int a, b;
-        a = 1;
-        b = 2;
-        int c = a + b;
+        int a = rec(5, 1);
+        Console.WriteLine("x = " + a);
         Console.WriteLine("INFO: Program finish");
     }
 
